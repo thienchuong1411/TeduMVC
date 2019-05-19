71c37951cb73f8b959957e1f342e25cd3d0e5dc4
thienchuong1411nn2 <35033951+thienchuong1411nn2@users.noreply.github.com>
2019-05-19 19:10:20 +0700

    Completed login page with User table on OnlineShop5 db

---
+++ OnlineShop/Areas/Admin/Code/CustomMembershipProvider.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Web.Security;  // for MembershipProvider
+using Models;   // for UserModel.Login method
+
+namespace OnlineShop.Areas.Admin.Code
+{
+    public class CustomMembershipProvider : MembershipProvider
+    {
+        public override bool EnablePasswordRetrieval => throw new NotImplementedException();
+
+        public override bool EnablePasswordReset => throw new NotImplementedException();
+
+        public override bool RequiresQuestionAndAnswer => throw new NotImplementedException();
+
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int MaxInvalidPasswordAttempts => throw new NotImplementedException();
+
+        public override int PasswordAttemptWindow => throw new NotImplementedException();
+
+        public override bool RequiresUniqueEmail => throw new NotImplementedException();
+
+        public override MembershipPasswordFormat PasswordFormat => throw new NotImplementedException();
+
+        public override int MinRequiredPasswordLength => throw new NotImplementedException();
+
+        public override int MinRequiredNonAlphanumericCharacters => throw new NotImplementedException();
+
+        public override string PasswordStrengthRegularExpression => throw new NotImplementedException();
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            // edit sử dụng method này. Để ý nó dùng bool trả về tương ứng với hàm Login ta xây dựng trong UserModel của Models project
+
+            return new UserModel().Login(username, password); // để ý cái chổ UserModel có cặp dấu ()
+        }
+    }
+}
--- OnlineShop/Areas/Admin/Controllers/LoginController.cs
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using Models;
 using OnlineShop.Areas.Admin.Code;
+using System.Web.Security;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -14,26 +15,55 @@ namespace OnlineShop.Areas.Admin.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Index(LoginModel model)
+        //{
+        //    // check if end user have input username by [Required] property in LoginModel
+        //    if(ModelState.IsValid)
+        //    {
+        //        var result = new UserModel().Login(model.UserName, model.Password);
+        //        if(result)
+        //        {
+        //            // luu session
+        //            //UserSession userSession = new UserSession();
+        //            //userSession.UserName = model.UserName;
+
+        //            // 2 lệnh trên có thể viết thành 1 dòng như sau:
+        //            UserSession userSession = new UserSession() { UserName = model.UserName };
+        //            SessionHelper.SetSession(userSession);
+
+        //            // 2 lệnh trên có thể viết rút gọn như sau
+        //            // SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError("", "Username or password is wrong");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        ModelState.AddModelError("", "Please input UserName");
+        //    }
+        //    return View(model);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Index(LoginModel model)
         {
             // check if end user have input username by [Required] property in LoginModel
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                var result = new UserModel().Login(model.UserName, model.Password);
-                if(result)
-                {
-                    // luu session
-                    //UserSession userSession = new UserSession();
-                    //userSession.UserName = model.UserName;
-                    
-                    // 2 lệnh trên có thể viết thành 1 dòng như sau:
-                    UserSession userSession = new UserSession() { UserName = model.UserName };
-                    SessionHelper.SetSession(userSession);
+                //var result = new UserModel().Login(model.UserName, model.Password);
 
-                    // 2 lệnh trên có thể viết rút gọn như sau
-                    // SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                bool result = new CustomMembershipProvider().ValidateUser(model.UserName, model.Password);
+                if (result)
+                {
+                    // lúc này ta dùng set cookie của Membership thay thế. Vì chút phải viết method Logout tương ứng
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
 
                     return RedirectToAction("Index", "Home");
                 }
@@ -47,6 +77,12 @@ namespace OnlineShop.Areas.Admin.Controllers
                 ModelState.AddModelError("", "Please input UserName");
             }
             return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
