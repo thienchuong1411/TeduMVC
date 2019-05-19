789d9d5a34dddfae0833c7baf65091b9965afa16
thienchuong1411nn2 <35033951+thienchuong1411nn2@users.noreply.github.com>
2019-05-19 08:57:40 +0700

    Add project files.

---
+++ HelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            // cách sử dụng ViewBag
+            ViewBag.MessageString = "Welcome to MVC";
+            var message = new MessageModel();
+            message.Welcome = "Hello Chuong";
+            return View(message);
+        }
+    }
+}
